361f5151fb28054158b388d043128b31effc306f
lm902 <i@lm902.cn>
2020-04-30 21:04:52 -0700

    Add disable user

--- app/model.ts
+++ app/model.ts
@@ -80,12 +80,14 @@ export interface IUser extends mongoose.Document {
   username: string
   password: string
   roles?: string[]
+  disabled?: boolean
 }
 
 export const User = mongoose.model<IUser>('User', new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
-  roles: [String]
+  roles: [String],
+  disabled: Boolean
 }))
 
 export interface IDoctor extends mongoose.Document {
