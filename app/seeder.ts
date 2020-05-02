e7bb2bac52f8012baa9c3ca586e85eee605b8423
Sebastian- <hmurgu@hotmail.com>
2020-05-02 09:02:06 -0700

    seed doctors #21

--- app/seeder.ts
+++ app/seeder.ts
@@ -7,8 +7,10 @@ import { Clinic } from './model/clinic'
 import { clinics } from './seed-data/clinics'
 import { Patient } from './model/patient'
 import { patients } from './seed-data/patients'
+import { Doctor } from './model/doctor'
+import { doctors } from './seed-data/doctors'
 
-export default async function seedDatabase(config: EggAppConfig) {
+export default async function seedDatabase (config: EggAppConfig) {
   const users = await seedUsers(config)
   const clinics = await seedClinics(
     users
@@ -16,10 +18,10 @@ export default async function seedDatabase(config: EggAppConfig) {
       .reduce((acc, curr) => ({ ...acc, [curr.username]: curr._id }), {})
   )
   const patients = await seedPatients()
-  console.log(clinics, patients)
-  // seed doctors
-  // seed appointments
+  const doctors = await seedDoctors()
+  console.log(clinics, patients, doctors)
   // seed form fields
+  // seed appointments
 }
 
 const seedUsers = async (config) => {
@@ -43,4 +45,8 @@ const seedClinics = async (admins: idMap) => {
 
 const seedPatients = async () => {
   return Patient.create(patients)
+}
+
+const seedDoctors = async () => {
+  return Doctor.create(doctors)
 }
