630cc25441091b448ef1e2de56fbe4dd76e4607c
Sebastian- <hmurgu@hotmail.com>
2020-05-02 10:56:36 -0700

    seed check-in form fields #21

--- app/seeder.ts
+++ app/seeder.ts
@@ -2,25 +2,31 @@ import { users } from './seed-data/users'
 import { User } from './model/user'
 import { EggAppConfig } from 'egg'
 import { sm3 } from 'sm-crypto'
-import { Schema } from 'mongoose'
-import { Clinic } from './model/clinic'
+// import { Schema } from 'mongoose'
+// import { Clinic } from './model/clinic'
 import { clinics } from './seed-data/clinics'
 import { Patient } from './model/patient'
 import { patients } from './seed-data/patients'
 import { Doctor } from './model/doctor'
 import { doctors } from './seed-data/doctors'
+import {
+  InputType,
+  inputName,
+  inputLabel,
+  CheckInFormField
+} from './model/checkinformfield'
 
 export default async function seedDatabase (config: EggAppConfig) {
   const users = await seedUsers(config)
-  const clinics = await seedClinics(
-    users
-      .filter((user) => user.roles?.includes('clinic'))
-      .reduce((acc, curr) => ({ ...acc, [curr.username]: curr._id }), {})
-  )
+  const checkInFormFields = seedCheckInFormFields()
+  // const clinics = await seedClinics(
+  //   users
+  //     .filter((user) => user.roles?.includes('clinic'))
+  //     .reduce((acc, curr) => ({ ...acc, [curr.username]: curr._id }), {})
+  // )
   const patients = await seedPatients()
   const doctors = await seedDoctors()
-  console.log(clinics, patients, doctors)
-  // seed form fields
+  console.log(users, checkInFormFields, clinics, patients, doctors)
   // seed appointments
 }
 
@@ -33,15 +39,15 @@ const seedUsers = async (config) => {
   )
 }
 
-type idMap = { [key: string]: Schema.Types.ObjectId }
-const seedClinics = async (admins: idMap) => {
-  return Clinic.create(
-    clinics.map((clinic) => ({
-      ...clinic,
-      ownerId: admins[clinic.ownerId]
-    }))
-  )
-}
+// type idMap = { [key: string]: Schema.Types.ObjectId }
+// const seedClinics = async (admins: idMap) => {
+//   return Clinic.create(
+//     clinics.map((clinic) => ({
+//       ...clinic,
+//       ownerId: admins[clinic.ownerId]
+//     }))
+//   )
+// }
 
 const seedPatients = async () => {
   return Patient.create(patients)
@@ -49,4 +55,18 @@ const seedPatients = async () => {
 
 const seedDoctors = async () => {
   return Doctor.create(doctors)
+}
+
+const seedCheckInFormFields = async () => {
+  const inputFields: {}[] = []
+
+  for (const field in InputType) {
+    inputFields.push({
+      inputType: field,
+      name: inputName(field as InputType),
+      label: inputLabel(field as InputType)
+    })
+  }
+
+  CheckInFormField.create(inputFields)
 }
