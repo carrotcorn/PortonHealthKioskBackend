5bb9c6829b3a00b52247361c048aeaad39025009
Sebastian- <hmurgu@hotmail.com>
2020-05-02 10:55:57 -0700

    add check-in form fields to database #21

--- app/model/checkinformfield.ts
+++ app/model/checkinformfield.ts
@@ -10,14 +10,61 @@ export enum InputType {
   POSTAL_CODE = 'POSTAL_CODE'
 }
 
-export interface ICheckInFormField {
+export function inputName (type: InputType) {
+  switch (type) {
+    case InputType.FIRST_NAME:
+      return 'givenName'
+    case InputType.LAST_NAME:
+      return 'familyName'
+    case InputType.BIRTHDAY:
+      return 'birthday'
+    case InputType.STREET_ADDRESS:
+      return 'street'
+    case InputType.CITY:
+      return 'city'
+    case InputType.PROVINCE:
+      return 'province'
+    case InputType.POSTAL_CODE:
+      return 'postcode'
+    default:
+      throw Error(`invalid input type: ${type}`)
+  }
+}
+
+export function inputLabel (type: InputType) {
+  switch (type) {
+    case InputType.FIRST_NAME:
+      return 'First Name'
+    case InputType.LAST_NAME:
+      return 'Last Name'
+    case InputType.BIRTHDAY:
+      return 'Birthday'
+    case InputType.STREET_ADDRESS:
+      return 'Street'
+    case InputType.CITY:
+      return 'City'
+    case InputType.PROVINCE:
+      return 'Province'
+    case InputType.POSTAL_CODE:
+      return 'Postal Code'
+    default:
+      throw Error(`invalid input type: ${type}`)
+  }
+}
+
+export interface ICheckInFormField extends mongoose.Document {
   inputType: InputType
   name: string
   label: string
 }
 
-export const CheckInFormField = new mongoose.Schema<ICheckInFormField>({
+export const CheckInFormFieldSchema = new mongoose.Schema<ICheckInFormField>({
   inputType: { type: String, required: true },
   name: { type: String, required: true },
   label: { type: String, required: true }
 })
+
+export const CheckInFormField = mongoose.model<ICheckInFormField>(
+  'CheckInFormField',
+  CheckInFormFieldSchema
+)
